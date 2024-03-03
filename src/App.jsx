8f5aee32613134a9edfb1c7668095cedb8d81ecb
braverokmc79 <braverokmc79@gmail.com>
2024-03-03 18:02:03 +0900

    154.프로젝트 생성 핸들링 & UI 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,47 @@ function App() {
     projects: [],
   });
 
-  function handleStartAddProject(cancel) {
+  function handleStartAddProject() {
     //여기서 null 은 프로젝트를 새롭게 추가한다는 것.
     setProjectsState((prevState) => {
       return {
         ...prevState,
-        selectedProjectId: cancel? undefined:null,
+        selectedProjectId: null,
       };
     });
   }
 
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState=>{
+      const proejctId=Math.random();
+      const newProject ={
+        ...projectData,
+        id:proejctId
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    });
+
+  }
+
+  console.log("projectsState ", projectsState);
+
+
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onStartAddProject={handleStartAddProject} />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelect onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}  projects={projectsState.projects}   />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,27 +1,53 @@
+import React, { useEffect, useRef } from "react";
+import Input from "./Input";
 
-import React from 'react'
-import Input from './Input';
+const NewProject = ({ onAdd }) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
 
-const NewProject = ({}) => {
+  function handleSave() {
+    const enteredTitle=title.current.value;
+    const enteredDescription=description.current.value;
+    const enteredDueDate=dueDate.current.value;
+    
+    //유효성 체크
+
+
+
+    const inputData={
+      title:enteredTitle,
+      description:enteredDescription,
+      dueDate:enteredDueDate
+    }
+    onAdd(inputData);
+  }
+
   return (
-    <div className='w-[35rem] mt-16'>
-        <menu className='flex items-center justify-end gap-4 my-4'>
-            <li>
-              <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
-            </li>
-            <li>
-              <button className='px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950  rounded-md'>Save</button>
-            </li>
-        </menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950  rounded-md"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </li>
+      </menu>
 
-        <div>
-            <Input label="Title" />
-            <Input label="Description"  textarea  />
-            <Input label="Due Date" />
-
-        </div>
+      <div>
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date"  ref={dueDate} label="Due Date" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NewProject;
