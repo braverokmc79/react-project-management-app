c609f140a7ba67239820ddf769d4c8c456904f06
braverokmc79 <braverokmc79@gmail.com>
2024-03-03 19:59:45 +0900

    157.선택 가능한 프로젝트 구현 및 프로젝트 정보 보기

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelect from "./components/NoProjectSelect";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -9,6 +10,7 @@ function App() {
     projects: [],
   });
 
+  //1.프로젝트 등록 화면 이동
   function handleStartAddProject() {
     //여기서 null 은 프로젝트를 새롭게 추가한다는 것.
     setProjectsState((prevState) => {
@@ -19,7 +21,17 @@ function App() {
     });
   }
 
+  //2.취소 버튼 클릭시 
+  function handleCancelAddProject(){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
 
+  //3.프로젝트 등록
   function handleAddProject(projectData){
     setProjectsState(prevState=>{
       const proejctId=Math.random();
@@ -37,19 +49,36 @@ function App() {
 
   }
 
-  console.log("projectsState ", projectsState);
+  //4. 메뉴에서 프로젝트 선택시 id 값이 존재
+  function handleSelectProject(id){
+    setProjectsState((prevState) => {
+      return {
+      ...prevState,
+         selectedProjectId: id,
+      };
+    });
+  }
+
+  const selectedProejct =projectsState.projects.find(project=>project.id === projectsState.selectedProjectId);
+
+  console.log("selectedProejct ", selectedProejct);
+  let content=selectedProejct &&  <SelectedProject   project={selectedProejct}  />
 
 
-  let content;
+
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = <NewProject onAdd={handleAddProject}  onCancel={handleCancelAddProject} />;
+
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelect onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject}  projects={projectsState.projects}   />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} 
+          projects={projectsState.projects}  
+          onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd , onCancel}) => {
   const modal=useRef();
   const title = useRef();
   const description = useRef();
@@ -32,15 +32,15 @@ const NewProject = ({ onAdd }) => {
   return (
     <>
       <Modal  ref={modal} buttonCaption="Okay">
-        <h2>Invalid Input</h2>
-        <p>Ooops ...looks like you forgot to ener a value. </p>
-        <p>Please mak sure you provide a valid value for every input field.</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className='text-stone-600 mb-4'>Ooops ...looks like you forgot to ener a value. </p>
+        <p className='text-stone-600 mb-4'>Please mak sure you provide a valid value for every input field.</p>
       </Modal>
 
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">
+          <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
             Cancel
           </button>
         </li>
